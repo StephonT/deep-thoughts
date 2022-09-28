88c4cfc9c57aca2f0bbd4a07fecb21094b72fb0b
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-09-28 19:56:21 -0400

    completed client folder

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
+import ThoughtList from '../components/ThoughtList';
+
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../utils/queries';
+
 const Home = () => {
+  // use useQuery hook to make query request
+  //with the loading property, we'll be able to conditionally render data based on whether or not there is data to even display
+  const { loading, data } = useQuery(QUERY_THOUGHTS);
+  const thoughts = data?.thoughts || [];
+  console.log(thoughts);
+
   return (
     <main>
-      <div className='flex-row justify-space-between'>
-        <div className='col-12 mb-3'>{/* PRINT THOUGHT LIST */}</div>
-      </div>
-    </main>
+  <div className="flex-row justify-space-between">
+    <div className="col-12 mb-3">
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
+      )}
+    </div>
+  </div>
+</main>
   );
 };
 
